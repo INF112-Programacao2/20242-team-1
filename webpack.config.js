09c5d8858eb88400d9a0e11d07e0e8b60bb8f7ec
SabrinaDoois <sabrina@doo.is>
2025-01-16 11:15:17 -0300

    feat: melhorar git ignore

--- webpack.config.js
+++ webpack.config.js
@@ -41,6 +41,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, 'dist', 'index.html'), // Caminho correto para o arquivo HTML
+      inject: false,
     }),
     // new BundleAnalyzerPlugin(),
 
