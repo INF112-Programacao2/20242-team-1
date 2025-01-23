264aeb7d792b5bb9d3672b878fb8b0d7a10ad290
Thamire334 <thamires.paes@ufv.br>
2025-01-23 15:59:42 -0300

    Classe Card

--- api/card.h
+++ api/card.h
@@ -6,25 +6,44 @@
 
 class Card{
     private:
-        int id;             // Identificador
-        std::string front;  // Frente da carta
-        std::string back;   // Verso da carta
-        // int deck_id;        // Baralho ao qual a carta pertence
-        // int levelTime_id;   // Nível de tempo ao qual a carta pertence
-        Date lastReview; // Última vez que a carta foi estudada
-        
-        // ----------------- //
-        // Sugestão: usar a biblioteca <ctime> para manipulação de datas,
-        // mas separado em outra classe - Data.h e Data.cpp
-        // MAIOR ACOPLAMENTO ENTRE AS CLASSES
-        // Info: https://www.cplusplus.com/reference/ctime/
-        // ----------------- //
+        int id;                     // Identificador
+        std::string front;          // Frente da carta
+        std::string back;           // Verso da carta
+        int deck_id;                // Baralho ao qual a carta pertence
+        int levelTime_id = NULL;    // Nível de tempo ao qual a carta pertence
+        Date lastReview;            // Última vez que a carta foi estudada
 
     public:
         Card();
+        Card(std::string front, std::string back, int deck_id);
+        ~Card() = default;
 
-        // Getters
-        //int getId() const;
-}; // @TODO
+        // ======= Getters =======
+
+        int getId() const;
+        std::string getFront() const;
+        std::string getBack() const;
+        int getDeckId() const;
+        int getLevelTimeId() const;
+        Date getLastReview() const;
+
+        // ======= Setters =======
+
+        void setId(int id);
+        void setFront(std::string front);
+        void setBack(std::string back);
+        void setDeckId(int deck_id);
+        void setLevelTimeId(int levelTime_id);
+        void setLastReview(Date lastReview);
+
+        // ======= Methods =======
+
+        void updateCard(std::string front, std::string back, int deck_id);
+        // virtual void setNextReview(int levelTime_id, Date lastReview) = 0;
+        // virtual void setNextReview() = 0;
+        // virtual LevelTime getNextLevelTime() = 0;
+
+
+}; 
 
 #endif // CARD_H
